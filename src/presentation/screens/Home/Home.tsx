cec2edf483ffc740998bedacb7ac6689df55c1a6
Hiago Mendes <hiagophd@gmail.com>
2025-01-16 15:35:26 -0300

    feat(home): enhance Home screen with user greeting, search functionality, and improved layout

--- src/presentation/screens/Home/Home.tsx
+++ src/presentation/screens/Home/Home.tsx
@@ -1,14 +1,121 @@
-import React from 'react';
-import { Container, TextContainer, Title } from './styles';
-import { StatusBar } from 'react-native';
+import React, { useContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import { Alert, FlatList, StatusBar, Text, TouchableOpacity } from 'react-native';
+import { UserContext } from '../../context/userContext';
+import { CarsService } from '../../../services/CarsService';
+import { GetMarksResponse } from '../../../services/CarsService/ICarsService';
+import { Input } from '../../components';
+import {
+  LogoutIcon,
+  CarBox,
+  CarName,
+  ChevronRight,
+  Container,
+  Header,
+  Subtitle,
+  TextContainer,
+  Title,
+  HighlightedTitle,
+} from './styles';
 
-const Home: React.FC = ({ route }: any) => {
+const logoutIcon = require('../../assets/logout.png');
+const chevronRight = require('../../assets/chevron-right.png');
+
+const Home: React.FC = () => {
+  const { user } = useContext(UserContext);
+  const navigation = useNavigation<any>();
+
+  const [userName, setUserName] = useState<string>('');
+  const [cars, setCars] = useState<GetMarksResponse[] | null>(null);
+  const [searchText, setSearchText] = useState<string>('');
+
+  const handleExitApp = async () => {
+    Alert.alert(
+      'Confirmação de Logout',
+      'Tem certeza que gostaria de sair?',
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Sim',
+          onPress: async () => {
+            await AsyncStorage.removeItem('userName');
+            navigation.reset({
+              index: 0,
+              routes: [{ name: 'SignIn' }],
+            });
+          },
+        },
+      ],
+      { cancelable: true }
+    );
+  };
+
+  const handleSelectModel = (model: string, name: string) => {
+    navigation.navigate('Model', { model, name });
+  };
+
+  const handleSearch = (text: string) => {
+    setSearchText(text);
+  };
+
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const storedUserName = await AsyncStorage.getItem('userName');
+      if (storedUserName) {
+        setUserName(storedUserName);
+      } else if (user) {
+        setUserName(user.name);
+      }
+    };
+
+    getUserInfo();
+  }, [user]);
+
+  useEffect(() => {
+    const getMarks = async () => {
+      const response: any = await new CarsService().getMarks();
+      setCars(response);
+    };
+
+    getMarks();
+  }, []);
+
+  const filteredCars = cars?.filter((car) =>
+    car.nome.toLowerCase().includes(searchText.toLowerCase())
+  );
+
+  const renderItem = ({ item }: { item: GetMarksResponse }) => (
+    <CarBox onPress={() => handleSelectModel(item.codigo, item.nome)}>
+      <CarName>{item.nome}</CarName>
+      <ChevronRight source={chevronRight} />
+    </CarBox>
+  );
+
   return (
     <Container>
-      <StatusBar barStyle="light-content" />
+      <StatusBar barStyle="dark-content" backgroundColor="#1C1C1C" />
+      <Header>
+        <Title>Bem-vindo, {userName}!</Title>
+        <TouchableOpacity onPress={handleExitApp}>
+          <LogoutIcon source={logoutIcon} />
+        </TouchableOpacity>
+      </Header>
+
       <TextContainer>
-        <Title>Home Page</Title>
+        <HighlightedTitle>Descubra seu próximo carro!</HighlightedTitle>
+        <Subtitle>{userName}, encontre o carro ideal entre as melhores marcas do mundo!</Subtitle>
+        <Input
+          onChangeText={handleSearch}
+          placeholder="Digite uma marca desejeda"
+          secureText={false}
+          value={searchText}
+        />
       </TextContainer>
+      <FlatList data={filteredCars} renderItem={renderItem} keyExtractor={(item) => item.codigo} />
     </Container>
   );
 };
--- src/presentation/screens/Home/styles.ts
+++ src/presentation/screens/Home/styles.ts
@@ -2,22 +2,67 @@ import styled from 'styled-components/native';
 
 export const Container = styled.ScrollView`
   flex: 1;
-  background-color:rgb(0, 0, 0);
+  background-color:#1C1C1C;
 `;
 
-export const TextContainer = styled.View`
-  padding-left: 24px;
-  padding-right: 24px;
+export const Header = styled.View`
+  height: 80px;
+  background-color: #F5F5F5;
+  padding: 20px 24px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
 `;
 
+
+export const LogoutIcon = styled.Image`
+  width: 30px;
+  height: 30px;
+`;
 
 export const Title = styled.Text`
-  color: #fff;
-  font-size: 24px;
+  color: #1C1C1C;
+  font-size: 18px;
   font-weight: 700;
-  text-align: center;
-  margin-top: 24px;
-  margin-bottom: 24px;
 `;
 
+export const HighlightedTitle = styled.Text`
+  color: #F5F5F5;
+  text-align: center;
+  font-size: 24px;
+  margin-top: 17px;
+  font-weight: 700;
+`;
+export const TextContainer = styled.View`
+  padding-horizontal: 24px;
+  margin-bottom: 20px;
+`;
 
+export const Subtitle = styled.Text`
+  color: #F5F5F5;
+  font-size: 16px;
+  font-weight: 300;
+  margin-bottom: 10px;
+  text-align: center;
+`;
+
+export const CarBox = styled.TouchableOpacity`
+  background-color:#F5F5F5;
+  border-radius: 10px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 16px 24px;
+  padding: 16px;
+`;
+
+export const CarName = styled.Text`
+  color: #2c2b34;
+  font-size: 16px;
+  font-weight: 700;
+`;
+
+export const ChevronRight = styled.Image`
+  width: 20px;
+  height: 20px;
+`;
